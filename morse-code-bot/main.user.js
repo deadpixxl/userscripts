9509a940786b5b8072e10390d29a9688e446294d
Felix Packard <fjpackard@gmail.com>
2019-09-26 20:25:26 +0100

    Updated version

--- morse-code-bot/main.user.js
+++ morse-code-bot/main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Morse Code Bot
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  A bot to send morse code messages on morsecode.me
 // @author       deadpixl
 // @match        http://morsecode.me/*
